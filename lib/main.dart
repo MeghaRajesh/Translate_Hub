f9a905fb2e30ee026361357b8a4f1b3a3fcf573a
MeghaRajesh <83894072+MeghaRajesh@users.noreply.github.com>
2023-11-14 22:39:04 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:translator/translator.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Translator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TranslatorScreen(),
+    );
+  }
+}
+
+class TranslatorScreen extends StatefulWidget {
+  @override
+  _TranslatorScreenState createState() => _TranslatorScreenState();
+}
+
+class _TranslatorScreenState extends State<TranslatorScreen> {
+  final translator = GoogleTranslator();
+  final stt.SpeechToText speech = stt.SpeechToText();
+  final FlutterTts flutterTts = FlutterTts();
+  String translatedText = '';
+  bool isListening = false;
+
+  void translateText(String text) async {
+    Translation translation = await translator.translate(text, to: 'hi'); // Translate to Hindi
+    setState(() {
+      translatedText = translation.text;
+    });
+  }
+
+  void startListening() async {
+    if (await speech.initialize()) {
+      setState(() {
+        isListening = true;
+      });
+      speech.listen(
+        onResult: (result) {
+          translateText(result.recognizedWords);
+        },
+      );
+    }
+  }
+
+  void stopListening() {
+    speech.stop();
+    setState(() {
+      isListening = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speech.cancel();
+    flutterTts.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Translator App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Translated Text:',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Text(
+            translatedText,
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+              // Speak the translated text in Hindi when the button is pressed
+              flutterTts.setLanguage('hi-IN'); // Set the language to Hindi
+              flutterTts.speak(translatedText);
+            },
+            child: Text('Speak'),
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              isListening
+                  ? IconButton(
+                      icon: Icon(Icons.stop),
+                      onPressed: stopListening,
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.mic),
+                      onPressed: startListening,
+                    ),
+              Text(
+                isListening ? 'Listening...' : 'Hold to Record',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
