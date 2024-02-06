87ec958d12afc105503e8667ae52ee496fe2f759
MeghaRajesh <megharajesh2024@cs.sjcetpalai.ac.in>
2024-02-06 21:01:38 +0530

    feb6

--- lib/option.dart
+++ lib/option.dart
@@ -5,9 +5,7 @@ class TranslatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TranslateHub'),
-      ),
+      
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
