c557f63268b555ad6ddedb5d9e773577f273f376
Saranya2909 <83824767+Saranya2909@users.noreply.github.com>
2024-02-13 23:15:52 +0530

    Feb 13

---
+++ lib/edit_profile.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  String _name = "John Doe";
+  String _address = "123 Main Street";
+  String _email = "johndoe@example.com";
+  String _phoneNumber = "+1234567890";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [const Color.fromARGB(255, 83, 34, 223), Colors.white],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    print('Change profile photo');
+                  },
+                  child: Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage('images/profilepic.jpg'),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {
+                            print('Change profile photo');
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(height: 25),
+                        SizedBox(width: 20), // Add space before the "Name" label
+                        Text(
+                          'Name',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //SizedBox(height: 0), // Add space between the "Name" label and the text
+                    TextFormField(
+                      initialValue: _name,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      ),
+                      cursorColor: Colors.black, // Set cursor color to black
+                      onChanged: (value) {
+                        setState(() {
+                          _name = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              //SizedBox(height: 10), // Add space after the text box
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(height: 25),
+                        SizedBox(width: 20), // Add space before the "Address" label
+                        Text(
+                          'Address',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //SizedBox(height: 10), // Add space between the "Address" label and the text
+                    TextFormField(
+                      initialValue: _address,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      ),
+                      cursorColor: Colors.black, // Set cursor color to black
+                      onChanged: (value) {
+                        setState(() {
+                          _address = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              //SizedBox(height: 10), // Add space after the text box
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(height: 25),
+                        SizedBox(width: 20), // Add space before the "Email" label
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //SizedBox(height: 10), // Add space between the "Email" label and the text
+                    TextFormField(
+                      initialValue: _email,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      ),
+                      cursorColor: Colors.black, // Set cursor color to black
+                      onChanged: (value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              //SizedBox(height: 10), // Add space after the text box
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(height: 25),
+                        SizedBox(width: 20), // Add space before the "Phone Number" label
+                        Text(
+                          'Phone Number',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //SizedBox(height: 10), // Add space between the "Phone Number" label and the text
+                    TextFormField(
+                      initialValue: _phoneNumber,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      ),
+                      cursorColor: Colors.black, // Set cursor color to black
+                      onChanged: (value) {
+                        setState(() {
+                          _phoneNumber = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Updated Profile:');
+                    print('Name: $_name');
+                    print('Address: $_address');
+                    print('Email: $_email');
+                    print('Phone Number: $_phoneNumber');
+                  },
+                  child: Text('Save Changes'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: EditProfilePage(),
+  ));
+}
---
+++ lib/option.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'voice.dart';
+import 'edit_profile.dart'; // Import the edit profile page
+import 'login.dart'; // Import the login page
+
+class TranslatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TranslateHub'),
+        leading: PopupMenuButton(
+          icon: Icon(Icons.menu),
+          itemBuilder: (BuildContext context) => [
+            PopupMenuItem(
+              child: ListTile(
+                leading: Icon(Icons.account_circle), // Add profile icon
+                title: Text('Profile'),
+              ),
+              value: 'profile',
+            ),
+            PopupMenuItem(
+              child: ListTile(
+                leading: Icon(Icons.logout), // Add logout icon
+                title: Text('Logout'),
+              ),
+              value: 'logout',
+            ),
+          ],
+          onSelected: (String value) {
+            if (value == 'profile') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => EditProfilePage()),
+              );
+            } else if (value == 'logout') {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => SignInPage()),
+              );
+            }
+          },
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [const Color.fromARGB(255, 83, 34, 223), Colors.white],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Image.asset(
+                'images/option.jpg', // Provide your image path here
+                width: 300,
+                height: 300,
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle chat option
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 100), // Adjust the padding as needed
+                ),
+                child: Text(
+                  'Chat',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18), // Adjust the font size as needed
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => VoiceTranslation()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 50), // Adjust the padding as needed
+                ),
+                child: Text(
+                  'Voice Translation',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18), // Adjust the font size as needed
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
