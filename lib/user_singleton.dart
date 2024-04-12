22401e02684667217fffe0582c0c0bd86be0fc8c
Saranya2909 <83824767+Saranya2909@users.noreply.github.com>
2024-04-12 10:35:20 +0530

    Delete lib/user_singleton.dart

--- lib/user_singleton.dart
+++
@@ -1,8 +0,0 @@
-class UserSingleton {
-  static final UserSingleton _instance = UserSingleton._internal();
-  factory UserSingleton() => _instance;
-  
-  UserSingleton._internal();
-
-  String userEmail = '';
-}
